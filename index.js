d4561db5146a1f76e2dfb8d016011a2340fe2b23
Nelly <turicenco@gmail.com>
2019-04-21 14:11:07 +0300

    Storage_Homewrok_Corrected

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-var userName = prompt("Please enter your name");
+// var userName = prompt("Please enter your name");
 
-if (userName.length <= 2) {
-    alert("Please try once again");
-} else {
-    window.localStorage.setItem('name', userName);
+// if (userName.length <= 2) {
+//     alert("Please try once again");
+// } else {
+//     window.localStorage.setItem('name', userName);
+// }
+
+// window.onload = function () {
+//     var storedValue = window.localStorage.getItem('name');
+//     if (storedValue !== null) {
+//         document.getElementById("myP").innerHTML = "Hello " + userName
+//     } else {
+//         document.getElementById("myP").innerHTML = "Hello guest";
+//     }
+// }
+
+var localStorageKey = "userName";
+
+function showMessage() {
+    var localStorageValue = localStorage.getItem(localStorageKey);
+
+    document.getElementById("myP").innerHTML = localStorageValue ? "Hello " + localStorageValue : "Hello guest";
 }
 
-window.onload = function () {
-    var storedValue = window.localStorage.getItem('name');
-    if (storedValue !== null) {
-        document.getElementById("myP").innerHTML = "Hello " + userName
+function handleReset() {
+    localStorage.removeItem(localStorageKey);
+
+    showMessage();
+}
+
+function handlePrompt() {
+    var userName = prompt("Please enter your name");
+
+    if (!userName) {
+        alert("Please try once again");
+        return;
+    } else if (userName.length <= 2) {
+        alert("Please try once again");
+        handleReset();
     } else {
-        document.getElementById("myP").innerHTML = "Hello guest";
+        window.localStorage.setItem(localStorageKey, userName);
     }
+
+    showMessage();
 }
+
+showMessage();
+
+document.getElementById('reset').addEventListener('click', handleReset);
+document.getElementById('prompt').addEventListener('click', handlePrompt);
